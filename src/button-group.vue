dd798e52ab0e64f3228f493418217d7f69643bb7
Lee <545360100@qq.com>
2019-02-13 18:11:17 +0800

    创建button-group

---
+++ src/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="c-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:'cButtonGroup'
+}
+</script>
+<style lang="scss">
+  .c-button-group{
+    display: inline-flex;
+    vertical-align: middle;
+    >.c-button{
+      border-radius: 0;
+      margin-left: -1px;
+      &:first-child{
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius:var(--border-radius);
+      }
+      &:last-child{
+        border-top-right-radius:var(--border-radius);
+        border-bottom-right-radius:var(--border-radius);
+      }
+      &:hover{
+        z-index: 1;
+      }
+    }
+  }
+</style>
+
+
