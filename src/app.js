abf4dd67c82b1c3a98299e95405e979e25c1b4fa
Lee <545360100@qq.com>
2019-02-18 00:44:54 +0800

    引入prettier 配置vscode 保存时候自动格式化代码

--- src/app.js
+++ src/app.js
@@ -4,106 +4,106 @@ import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
 
-Vue.component('c-icon',Icon)
-Vue.component('c-button',Button)
-Vue.component('c-button-group',ButtonGroup)
-Vue.component('c-input',Input)
+Vue.component('c-icon', Icon)
+Vue.component('c-button', Button)
+Vue.component('c-button-group', ButtonGroup)
+Vue.component('c-input', Input)
 
 new Vue({
-  el: '#app',
-  data: {
-    loading1:false
-  },
-  methods:{
-    clg(e){
-      console.log('你好啊')
-    }
-  }
+	el: '#app',
+	data: {
+		loading1: false
+	},
+	methods: {
+		clg(e) {
+			console.log('你好啊')
+		}
+	}
 })
 
 import chai from 'chai'
-import spies from 'chai-spies';
+import spies from 'chai-spies'
 chai.use(spies)
 
 const expect = chai.expect
 // 单元测试
 {
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'settings',
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href =useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-settings')
-  vm.$el.remove()
-  vm.$destroy()
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'settings'
+		}
+	})
+	vm.$mount()
+	let useElement = vm.$el.querySelector('use')
+	let href = useElement.getAttribute('xlink:href')
+	expect(href).to.eq('#i-settings')
+	vm.$el.remove()
+	vm.$destroy()
 }
 {
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'settings',
-      loading:true
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href =useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  vm.$el.remove()
-  vm.$destroy()
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'settings',
+			loading: true
+		}
+	})
+	vm.$mount()
+	let useElement = vm.$el.querySelector('use')
+	let href = useElement.getAttribute('xlink:href')
+	expect(href).to.eq('#i-loading')
+	vm.$el.remove()
+	vm.$destroy()
 }
 {
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'settings'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} =window.getComputedStyle(svg)  //不渲染没有加载css css所有属性值都是字符串
-  expect(order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'settings'
+		}
+	})
+	vm.$mount(div)
+	let svg = vm.$el.querySelector('svg')
+	let { order } = window.getComputedStyle(svg) //不渲染没有加载css css所有属性值都是字符串
+	expect(order).to.eq('1')
+	vm.$el.remove()
+	vm.$destroy()
 }
 {
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'settings',
-      iconPosition:'right'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} =window.getComputedStyle(svg)  //不渲染没有加载css css所有属性值都是字符串
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'settings',
+			iconPosition: 'right'
+		}
+	})
+	vm.$mount(div)
+	let svg = vm.$el.querySelector('svg')
+	let { order } = window.getComputedStyle(svg) //不渲染没有加载css css所有属性值都是字符串
+	expect(order).to.eq('2')
+	vm.$el.remove()
+	vm.$destroy()
 }
 {
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'settings',
-    }
-  })
-  vm.$mount()
-  let spy = chai.spy(function(){})
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'settings'
+		}
+	})
+	vm.$mount()
+	let spy = chai.spy(function() {})
 
-  vm.$on('click',spy)
-  let button = vm.$el
+	vm.$on('click', spy)
+	let button = vm.$el
 
-  button.click()
-  expect(spy).to.have.been.called()
-  // cButton.$el.remove()
-  // cButton.$destroy()
+	button.click()
+	expect(spy).to.have.been.called()
+	// cButton.$el.remove()
+	// cButton.$destroy()
 }
--- src/input.vue
+++ src/input.vue
@@ -15,34 +15,34 @@
 </template>
 
 <script>
-import cIcon from "./icon";
+import cIcon from './icon'
 
 export default {
-  components: { cIcon },
-  name: "cInput",
-  props: {
-    placeholder: {
-      type: String,
-      default: ""
-    },
-    value: {
-      type: String,
-      default: ""
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    readonly: {
-      type: Boolean,
-      default: false
-    },
-    error: {
-      type: String,
-      default: ""
-    }
-  }
-};
+	components: { cIcon },
+	name: 'cInput',
+	props: {
+		placeholder: {
+			type: String,
+			default: ''
+		},
+		value: {
+			type: String,
+			default: ''
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		readonly: {
+			type: Boolean,
+			default: false
+		},
+		error: {
+			type: String,
+			default: ''
+		}
+	}
+}
 </script>
 
 <style lang='scss' scoped>
@@ -54,40 +54,40 @@ $font-size: 14px;
 $box-shadow-color: rgba(0, 0, 0, 0.5);
 $red: #f1453d;
 .wrapper {
-  font-size: $font-size;
-  display: inline-block;
-  vertical-align: middle;
-  > input {
-    height: 32px;
-    border: 1px solid $border-color;
-    border-radius: $border-radius;
-    padding: 8px 9px;
-    font-size: inherit;
-    &:hover {
-      border-color: $border-color-hover;
-    }
-    &:focus {
-      outline: none;
-      box-shadow: 0 1px 3px $box-shadow-color inset;
-    }
-    &[disabled],
-    &[readonly] {
-      border-color: #ddd;
-      color: #ddd;
-      cursor: not-allowed;
-    }
-  }
-  &.error {
-    > input {
-      border-color: $red;
-    }
-  }
-  .icon-error {
-    fill: $red;
-  }
-  .errorMsg {
-    color: $red;
-  }
+	font-size: $font-size;
+	display: inline-block;
+	vertical-align: middle;
+	> input {
+		height: 32px;
+		border: 1px solid $border-color;
+		border-radius: $border-radius;
+		padding: 8px 9px;
+		font-size: inherit;
+		&:hover {
+			border-color: $border-color-hover;
+		}
+		&:focus {
+			outline: none;
+			box-shadow: 0 1px 3px $box-shadow-color inset;
+		}
+		&[disabled],
+		&[readonly] {
+			border-color: #ddd;
+			color: #ddd;
+			cursor: not-allowed;
+		}
+	}
+	&.error {
+		> input {
+			border-color: $red;
+		}
+	}
+	.icon-error {
+		fill: $red;
+	}
+	.errorMsg {
+		color: $red;
+	}
 }
 </style>
 
