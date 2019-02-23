31994eb32f7fd28c885cb82dff9ce64242210489
Lee <545360100@qq.com>
2019-02-23 20:01:25 +0800

    动画slider

--- src/sider.vue
+++ src/sider.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-show="showSider">
+      <slot></slot>
+      <button class="close" @click="showSider=!showSider">X</button>
+    </div>
+  </transition>
 </template>
 <script>
 export default {
-	name: 'cSider'
+	name: 'cSider',
+	data() {
+		return { showSider: true }
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 .sider {
+	position: relative;
+	& > .close {
+		width: 10%;
+		position: absolute;
+		top: 0;
+		right: 0;
+	}
+}
+.slide-enter-active,
+.slide-leave-active {
+	transition: all 0.3s;
+}
+.slide-enter, .slide-leave-to /* .slide-leave-active below version 2.1.8 */ {
+	margin-left: -200px;
 }
 </style>
