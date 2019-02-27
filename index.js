27048b9a2118146899ddfbed6647cf6e18045b3d
Lee <545360100@qq.com>
2019-02-27 22:44:19 +0800

    fixed

--- index.js
+++ index.js
@@ -2,5 +2,26 @@ import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
 import Input from './src/icon'
+import Layout from './src/layout'
+import Col from './src/col'
+import Content from './src/content'
+import Footer from './src/footer'
+import Header from './src/header'
+import Sider from './src/sider'
+import Row from './src/row'
+import Toast from './src/toast'
 
-export {Button,ButtonGroup,Icon,Input}
+export {
+	Button,
+	ButtonGroup,
+	Icon,
+	Input,
+	Layout,
+	Col,
+	Content,
+	Footer,
+	Header,
+	Sider,
+	Row,
+	Toast
+}
